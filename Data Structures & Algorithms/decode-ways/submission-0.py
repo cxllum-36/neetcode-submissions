a0226b5b71db64689e0f161b73bfd4b7498b0e30
cxllum-36 <98830499+cxllum-36@users.noreply.github.com>
2026-06-19 20:35:50 +0100

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp=[0]*(len(s)+1)
+        dp[-1]=1
+
+        for i in range(len(s)-1,-1,-1):
+            if s[i]=='0': continue
+            elif i+2 <= len(s) and 1 <= int(s[i] + s[i+1]) <=26:
+                dp[i]=dp[i+1]+dp[i+2]
+            else:
+                dp[i]=dp[i+1]
+        return dp[0]
+        
